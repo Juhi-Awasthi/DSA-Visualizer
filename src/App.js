211054ccdfdffa5ae57990d3685518d4a115a504
Juhi-Awasthi <awasthijuhi409@gmail.com>
2025-03-12 15:32:07 +0530

    added some changes

--- src/App.js
+++ src/App.js
@@ -27,13 +27,16 @@
 import React from "react";
 import SortingVisualizer from "./components/SortingVisualizer";
 import SearchingVisualizer from "./components/SearchingVisualizer";
+import SearchingVisualizerPage from "./pages/SearchingVisualizerPage";
 
 function App() {
   return (
     <div className="App">
       <h1>DSA Visualizer</h1>
       <SortingVisualizer />
-      <SearchingVisualizer />
+      <Link to="/searching">
+          <button>Open Binary Search</button>
+      </Link>
     </div>
   );
 }
---
+++ src/pages/SearchingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchingVisualizer from "../components/SearchingVisualizer";
+
+const SearchingVisualizerPage = () => {
+  return (
+    <div>
+      <h1>Binary Search Visualizer</h1>
+      <SearchingVisualizer />
+    </div>
+  );
+};
+
+export default SearchingVisualizerPage;
