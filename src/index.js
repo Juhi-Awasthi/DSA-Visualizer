7d01972e3363d338ba61146e8226bb8d7e2c286e
Juhi-Awasthi <awasthijuhi409@gmail.com>
2025-03-13 13:39:41 +0530

    done some changes in index.js

--- src/index.js
+++ src/index.js
@@ -16,18 +16,32 @@
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import App from "./App";
-import SearchingPage from "./pages/SearchingVisualizerPage";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import App from "./App";
+// import SearchingPage from "./pages/SearchingVisualizerPage";
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/search" element={<SearchingPage />} />
-    </Routes>
-  </Router>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <Router>
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="/search" element={<SearchingPage />} />
+//     </Routes>
+//   </Router>,
+//   document.getElementById("root")
+// );
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // Ensures global styles are applied
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
